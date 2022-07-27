b0c774ed03fd4e38339b9878c23b9d302ec3cbf8
ari3636 <110034493+ari3636@users.noreply.github.com>
2022-07-27 12:02:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('COINFANTASY');
+  res.send('COINFANTASY FEASTT');
 });
 
 app.listen(8080, () => {
